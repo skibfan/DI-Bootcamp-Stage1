7c392e5457df1bd48b695c1f3f4ab6cff0c727ca
kozlozyabr <111320992+kozlozyabr@users.noreply.github.com>
2024-08-05 17:50:15 +0300

    finished

---
+++ week1/day2/exercisesXP/ex5.py
@@ -0,0 +1,9 @@
+
+for i in range(21): 
+    print(i, end=" ")
+
+print("")
+
+for i in range(21): 
+    if i % 2 == 0:
+        print(i, end = " ")
---
+++ week1/day2/exercisesXP/ex8.py
@@ -0,0 +1,8 @@
+
+
+toppings = []
+temp = input("Let's start! Add something on your pizza: ")
+final_toppings = ""
+counter = 0
+
+while temp
