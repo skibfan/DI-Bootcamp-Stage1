ce0987e70b37fa555f974c35814773b433949753
kozlozyabr <111320992+skibfan@users.noreply.github.com>
2024-08-17 19:42:26 +0300

    github shenanigans

--- week2/day5/rock_paper_scissors/rock-paper-scissors.py
+++ week2/day5/rock_paper_scissors/rock-paper-scissors.py
@@ -1,14 +1,5 @@
 import game as g
 
-# print(g.Game.get_user_item())
-
-
-# g.Game.play()
-# gs = g.Game()
-# gs.play()
-# gs.play()
-
-
 def get_user_menu_choice():
     menu_choice = input("You are at the menu of our wonderful Rock-Paper-Scissors game\nPlease, choose your next step:\n Play a new game: 'g'\nShow scores: 'x'\nQuit the game and see results: 'q'\nYour choice: ")
     return menu_choice
@@ -21,7 +12,6 @@ def print_results(results):
  
 
 def main():
-    
     flag = True
     while flag:
         us_m = get_user_menu_choice()
@@ -33,7 +23,6 @@ def main():
         elif us_m == 'g':
             gs = g.Game()
             gs.play()
-        
 
 
 main()
