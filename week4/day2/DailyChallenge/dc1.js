a9e2ad793dbb4dab0e36ac9fd13cc443064bd458
kozlozyabr <111320992+skibfan@users.noreply.github.com>
2024-08-27 23:00:07 +0300

    finished

--- week4/day2/DailyChallenge/dc1.js
+++ week4/day2/DailyChallenge/dc1.js
@@ -1,3 +1,12 @@
 let sentence = '// If the word “bad” does not come after “not” or the words are not in the sentence, console.log the original sentence.'
 
-let wordNot =
+let wordNot = sentence.indexOf("not")
+let wordBad = sentence.indexOf("bad")
+
+if (wordBad > wordNot && wordBad != -1 && wordNot != -1) {
+    sentence = sentence.slice(0, wordNot)  + 'good' + sentence.slice(wordBad+3)
+}
+
+console.log(sentence)
+
+
