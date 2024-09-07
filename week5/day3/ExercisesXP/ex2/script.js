e98b1e0b20f7c61935213a9560aa0b9edda9820f
kozlozyabr <111320992+skibfan@users.noreply.github.com>
2024-09-07 23:25:10 +0300

    finished

---
+++ week5/day3/ExercisesXP/ex2/script.js
@@ -0,0 +1,8 @@
+function displayStudentInfo(objUser){
+    const {first, last} = objUser
+    return `Your full name is ${first} ${last}`
+}
+
+
+
+console.log(displayStudentInfo({first: 'Elie', last:'Schoppik'}));
---
+++ week5/day3/ExercisesXP/ex4/script.js
@@ -0,0 +1,12 @@
+class Person {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+  
+  const member = new Person('John');
+  console.log(typeof member);
+
+
+//   we created a variable of type class Person named John. 
+// the output to console would be "object"
